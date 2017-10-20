0bd4d8638ed8c020ffc0ebd3c594e9dca01afe59
Tyler Weeres <tweeres04@gmail.com>
2017-10-20 13:08:19 -0700

    PoC

---
+++ src/App.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import TimePicker from 'rc-time-picker';
+
+import setHours from 'date-fns/set_hours';
+import setMinutes from 'date-fns/set_minutes';
+import dateParse from 'date-fns/parse';
+import differenceInMinutes from 'date-fns/difference_in_minutes';
+import dateFormat from 'date-fns/format';
+
+import flow from 'lodash/fp/flow';
+
+import './App.css';
+
+export function drinkFactory({ time, value }) {
+	return {
+		time,
+		value
+	};
+}
+
+export function currentDrinks(drinks) {
+	const now = new Date();
+	const today = dateFormat(now, 'YYYY-MM-DD');
+	return drinks.reduce((drinks, drink) => {
+		const minsSinceDrink = differenceInMinutes(
+			now,
+			`${today}:${drink.time}`
+		);
+		const effectiveDrinks =
+			minsSinceDrink == 0
+				? 1
+				: minsSinceDrink > 59
+					? 0
+					: (1 - minsSinceDrink / 60) * drink.value;
+		drinks += effectiveDrinks;
+		return drinks;
+	}, 0);
+}
+
+class NewDrink extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			time: dateFormat(new Date(), 'HH:mm'),
+			value: 1
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	render() {
+		const { time, value } = this.state;
+		return (
+			<div className="new-drink">
+				<div className="time">
+					<input
+						name="time"
+						type="text"
+						value={time}
+						onChange={this.handleChange}
+					/>
+				</div>
+				<div className="value">
+					<input
+						name="value"
+						type="number"
+						value={value}
+						onChange={this.handleChange}
+					/>
+				</div>
+				<button onClick={this.handleSubmit}>Drink!</button>
+			</div>
+		);
+	}
+	handleChange({ target: { value, name } }) {
+		this.setState({ [name]: value });
+	}
+	handleSubmit() {
+		const { addDrink } = this.props;
+		const { time, value } = this.state;
+		addDrink(
+			drinkFactory({
+				time,
+				value
+			})
+		);
+	}
+}
+
+function Drinks({ drinks }) {
+	const drinkListItems = drinks.map((drink, i) => (
+		<li key={i}>{drink.time}</li>
+	));
+	return <ul>{drinkListItems}</ul>;
+}
+
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			drinks: []
+		};
+
+		this.addDrink = this.addDrink.bind(this);
+	}
+	render() {
+		const { drinks } = this.state;
+		return (
+			<div className="App">
+				<div className="current-drinks">
+					<div className="value">
+						{currentDrinks(drinks).toFixed(2)}
+					</div>
+					<div className="label">drinks in your system</div>
+				</div>
+				<NewDrink addDrink={this.addDrink} />
+				<Drinks drinks={drinks} />
+			</div>
+		);
+	}
+	addDrink(drink) {
+		this.setState(prevState => {
+			return {
+				drinks: prevState.drinks.concat(drink)
+			};
+		});
+	}
+}
---
+++ src/App.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import subMinutes from 'date-fns/sub_minutes';
+import dateFormat from 'date-fns/format';
+
+import App, { drinkFactory, currentDrinks, setTime } from './App';
+
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<App />, div);
+});
+
+it('creates a proper drinkFactory', () => {});
+
+it('gives the right number of current drinks', () => {
+	const format = 'HH:mm';
+	const nowDate = new Date();
+
+	const twoHoursAgo = dateFormat(subMinutes(nowDate, 120), format);
+	const fiftyFiveMinutesAgo = dateFormat(subMinutes(nowDate, 55), format);
+	const fortyFiveMinutesAgo = dateFormat(subMinutes(nowDate, 45), format);
+	const thirtyMinutesAgo = dateFormat(subMinutes(nowDate, 30), format);
+	const now = dateFormat(nowDate, format);
+
+	// console.log(now, thirtyMinutesAgo, fortyFiveMinutesAgo, twoHoursAgo);
+	const drinks = [
+		drinkFactory({
+			time: twoHoursAgo,
+			value: 1
+		}),
+		drinkFactory({
+			time: fiftyFiveMinutesAgo,
+			value: 1
+		}),
+		drinkFactory({
+			time: fortyFiveMinutesAgo,
+			value: 1
+		}),
+		drinkFactory({
+			time: thirtyMinutesAgo,
+			value: 1
+		}),
+		drinkFactory({
+			time: now,
+			value: 1
+		})
+	];
+
+	expect(currentDrinks(drinks)).toBeCloseTo(1.83);
+});
