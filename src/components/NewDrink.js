61fa73ec5b63a6c807db69030bf9cf7c75f4d950
Tyler Weeres <tweeres04@gmail.com>
2017-12-30 00:00:10 -0800

    New drink validation

--- src/components/NewDrink.js
+++ src/components/NewDrink.js
@@ -3,6 +3,7 @@ import TimePicker from 'react-datetime';
 import _toNumber from 'lodash/fp/toNumber';
 import subtractDays from 'date-fns/sub_days';
 import closestDate from 'date-fns/closest_to';
+import isDate from 'date-fns/is_date';
 
 import { drinkFactory, Section } from '../App';
 
@@ -12,7 +13,9 @@ export default class NewDrink extends Component {
 
 		this.state = {
 			time: new Date(),
-			value: 1
+			timeError: false,
+			value: 1,
+			valueError: false
 		};
 
 		this.handleTimeChange = this.handleTimeChange.bind(this);
@@ -20,40 +23,55 @@ export default class NewDrink extends Component {
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 	render() {
-		const { time, value } = this.state;
+		const { time, timeError, value, valueError } = this.state;
 
 		return (
 			<Section>
 				<label className="label">Time</label>
-				<div className="field has-addons">
-					<div className="control is-expanded">
-						<TimePicker
-							inputProps={{
-								className: 'input'
-							}}
-							dateFormat={false}
-							timeFormat="HH:mm"
-							value={time}
-							onChange={this.handleTimeChange}
-						/>
+				<div className="field">
+					<div
+						className="field has-addons"
+						style={{ marginBottom: 0 }}
+					>
+						<div className="control is-expanded">
+							<TimePicker
+								inputProps={{
+									className: `input${
+										timeError ? ' is-danger' : ''
+									}`
+								}}
+								dateFormat={false}
+								timeFormat="HH:mm"
+								value={time}
+								onChange={this.handleTimeChange}
+							/>
+						</div>
+						<div className="control">
+							<button className="button" onClick={this.now}>
+								Now
+							</button>
+						</div>
 					</div>
-					<div className="control">
-						<button className="button" onClick={this.now}>
-							Now
-						</button>
-					</div>
+					{timeError && (
+						<p className="help is-danger">Enter a valid time.</p>
+					)}
 				</div>
 				<div className="field">
 					<label className="label">Drinks</label>
 					<div className="control">
 						<input
-							className="input"
+							className={`input${valueError ? ' is-danger' : ''}`}
 							name="value"
 							type="number"
 							value={value}
 							onChange={this.handleChange}
 						/>
 					</div>
+					{valueError && (
+						<p className="help is-danger">
+							Enter a number greater than 0.
+						</p>
+					)}
 				</div>
 				<button
 					className="button is-primary is-fullwidth"
@@ -67,24 +85,30 @@ export default class NewDrink extends Component {
 	handleTimeChange(value) {
 		// Handle possibly going past midnight. Just pick whatever day is closest to now. Will need to test this further
 		let time = value.toDate ? value.toDate() : value;
-		const possibleDays = [time, subtractDays(time, 1)];
-		time = closestDate(new Date(), possibleDays);
-		this.setState({ time });
+		if (isDate(time)) {
+			const possibleDays = [time, subtractDays(time, 1)];
+			time = closestDate(new Date(), possibleDays);
+		}
+		this.setState({ time, timeError: false });
 	}
 	handleChange({ target: { value, name } }) {
 		value = value == '' ? value : _toNumber(value);
-		this.setState({ [name]: value });
+		this.setState({ [name]: value, valueError: false });
 	}
 	handleSubmit() {
 		const { addDrink } = this.props;
 		const { time, value } = this.state;
-		if (value > 0) {
+		const valueError = value <= 0;
+		const timeError = !isDate(time);
+		if (!timeError && !valueError) {
 			addDrink(
 				drinkFactory({
 					time,
 					value
 				})
 			);
+		} else {
+			this.setState({ timeError, valueError });
 		}
 	}
 	now = () => {
