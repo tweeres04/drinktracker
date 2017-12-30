8a1e227ff9f739c4710fed9c7af3e8b43583a843
Tyler Weeres <tweeres04@gmail.com>
2017-12-29 23:29:56 -0800

    Tiny bit of polish

--- src/App.js
+++ src/App.js
@@ -61,14 +61,15 @@ export function Section({ children, className }) {
 }
 
 function CurrentDrinks({ drinks, now }) {
+	const currentDrinksValue = currentDrinks({ drinks, now });
 	return (
 		<section className="hero is-primary">
 			<div className="hero-body">
 				<div className="container has-text-centered">
-					<h1 className="title">
-						{currentDrinks({ drinks, now }).toFixed(2)}
-					</h1>
-					<h2 className="subtitle">drinks in your system</h2>
+					<h1 className="title">{currentDrinksValue.toFixed(2)}</h1>
+					<h2 className="subtitle">
+						drink{currentDrinksValue == 1 ? '' : 's'} in your system
+					</h2>
 				</div>
 			</div>
 		</section>
