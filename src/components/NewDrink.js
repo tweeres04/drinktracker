7afc2fffc48308b69d5e518ebd275d6cc0cb19de
Tyler Weeres <tweeres04@gmail.com>
2018-01-27 14:06:01 -0800

    Store the drink value

--- src/components/NewDrink.js
+++ src/components/NewDrink.js
@@ -4,90 +4,107 @@ import _toNumber from 'lodash/fp/toNumber';
 import subtractDays from 'date-fns/sub_days';
 import closestDate from 'date-fns/closest_to';
 import isDate from 'date-fns/is_date';
+import idbKeyval from 'idb-keyval';
 
 import { drinkFactory, Section } from '../App';
 import DrinkAdder from './DrinkAdder';
 
+async function loadState() {
+	return Object.assign(
+		{
+			time: new Date(),
+			timeError: false,
+			value: 1,
+			valueError: false
+		},
+		{ loading: false },
+		{ value: await idbKeyval.get('drinkValue') }
+	);
+}
+
 export default class NewDrink extends Component {
 	state = {
-		time: new Date(),
-		timeError: false,
-		value: 1,
-		valueError: false
+		loading: true
 	};
-	componentDidMount = () => {
+	async componentDidMount() {
+		const state = await loadState();
+		this.setState(state);
 		document.addEventListener('visibilitychange', () => {
 			if (!document.hidden) {
 				this.now();
 			}
 		});
-	};
+	}
 	render() {
-		const { time, timeError, value, valueError } = this.state;
+		const { time, timeError, value, valueError, loading } = this.state;
 
 		return (
-			<Section>
-				<label className="label">Time</label>
-				<div className="field">
-					<div
-						className="field has-addons"
-						style={{ marginBottom: 0 }}
-					>
-						<div className="control is-expanded">
-							<TimePicker
-								inputProps={{
-									className: `input${
-										timeError ? ' is-danger' : ''
-									}`
-								}}
-								dateFormat={false}
-								timeFormat={true}
-								value={time}
-								onChange={this.handleTimeChange}
-							/>
+			loading || (
+				<Section>
+					<label className="label">Time</label>
+					<div className="field">
+						<div
+							className="field has-addons"
+							style={{ marginBottom: 0 }}
+						>
+							<div className="control is-expanded">
+								<TimePicker
+									inputProps={{
+										className: `input${
+											timeError ? ' is-danger' : ''
+										}`
+									}}
+									dateFormat={false}
+									timeFormat={true}
+									value={time}
+									onChange={this.handleTimeChange}
+								/>
+							</div>
+							<div className="control">
+								<button className="button" onClick={this.now}>
+									Now
+								</button>
+							</div>
 						</div>
-						<div className="control">
-							<button className="button" onClick={this.now}>
-								Now
-							</button>
-						</div>
+						{timeError && (
+							<p className="help is-danger">
+								Enter a valid time.
+							</p>
+						)}
 					</div>
-					{timeError && (
-						<p className="help is-danger">Enter a valid time.</p>
-					)}
-				</div>
-				<label className="label">Standard drinks</label>
-				<div className="field">
-					<div
-						className="field has-addons"
-						style={{ marginBottom: 0 }}
-					>
-						<div className="control is-expanded">
-							<input
-								className={`input${
-									valueError ? ' is-danger' : ''
-								}`}
-								name="value"
-								type="number"
-								value={value}
-								onChange={this.handleChange}
-							/>
+					<label className="label">Standard drinks</label>
+					<div className="field">
+						<div
+							className="field has-addons"
+							style={{ marginBottom: 0 }}
+						>
+							<div className="control is-expanded">
+								<input
+									className={`input${
+										valueError ? ' is-danger' : ''
+									}`}
+									name="value"
+									type="number"
+									value={value}
+									onChange={this.handleChange}
+								/>
+							</div>
+							<DrinkAdder setDrinks={this.setDrinks} />
 						</div>
-						<DrinkAdder setDrinks={this.setDrinks} />
+						{valueError && (
+							<p className="help is-danger">
+								Enter a number greater than 0.
+							</p>
+						)}
 					</div>
-					{valueError && (
-						<p className="help is-danger">
-							Enter a number greater than 0.
-						</p>
-					)}
-				</div>
-				<button
-					className="button is-primary is-fullwidth is-medium"
-					onClick={this.handleSubmit}
-				>
-					Drink
-				</button>
-			</Section>
+					<button
+						className="button is-primary is-fullwidth is-medium"
+						onClick={this.handleSubmit}
+					>
+						Drink
+					</button>
+				</Section>
+			)
 		);
 	}
 	handleTimeChange = value => {
@@ -102,6 +119,9 @@ export default class NewDrink extends Component {
 	handleChange = ({ target: { value, name } }) => {
 		value = value == '' ? value : _toNumber(value);
 		this.setState({ [name]: value, valueError: false });
+		if (name == 'value') {
+			idbKeyval.set('drinkValue', value);
+		}
 	};
 	setDrinks = drinks => {
 		this.setState({ value: drinks });
